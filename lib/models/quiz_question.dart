8448e182e9afb8f01aba9ba8369248647dfb178c
Ahmed Amin <ahmedamin100dev@gmail.com>
2024-10-26 18:41:13 +0300

    Refactor QuizQuestion class constructor for simplicity

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -1,8 +1,8 @@
 class QuizQuestion {
-  const QuizQuestion({
-    required this.question,
-    required this.answers,
-  });
+  const QuizQuestion(
+    this.question,
+    this.answers,
+  );
 
   final String question;
   final List<String> answers;
